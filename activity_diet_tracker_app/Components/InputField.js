6e66650bfa4c1217604f7a33f02dce43d2b8c4d7
Chenyu Jiang <jiang.chenyu@northeastern.edu>
2024-10-03 21:12:33 -0700

    added an input component and three input on add act screen

---
+++ activity_diet_tracker_app/Components/InputField.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React from 'react';
+import { Colors } from '../Utils/Colors';
+
+export default function InputField({ label, value, onChangeText, placeholder, keyboardType = 'default' }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        keyboardType={keyboardType}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 46,
+    marginBottom: 16,
+    width: '80%',
+    alignSelf: 'center',
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: Colors.primary,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    borderRadius: 4,
+    borderWidth: 2,
+  },
+});
--- activity_diet_tracker_app/Screens/AddAnActivityScreen.js
+++ activity_diet_tracker_app/Screens/AddAnActivityScreen.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import InputField from '../Components/InputField'
 
 export default function AddAnActivityScreen() {
   return (
     <View>
-      <Text>AddAnActivityScreen</Text>
+        <InputField label="Activity *" />
+        <InputField label="Duration (min) *" />
+        <InputField label="Date *" />
     </View>
   )
 }
