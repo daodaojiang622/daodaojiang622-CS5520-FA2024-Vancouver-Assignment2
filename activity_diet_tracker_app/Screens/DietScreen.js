d16e07f6e6f5c900b27a03577849fecbd554d2ab
CHENYU JIANG <146473993+daodaojiang622@users.noreply.github.com>
2024-10-12 23:11:01 -0700

    Delete activity_diet_tracker_app/Screens directory
    I dont know why but for some reason my main branch disappeared and now I am rebuilding

--- activity_diet_tracker_app/Screens/DietScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import ItemsList from '../Components/ItemsList';
-
-export default function DietScreen() {
-  return (
-    <ItemsList data={dietEntries} />
-  );
-}
